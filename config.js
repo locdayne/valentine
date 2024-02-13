4b7bc5a8f87e661ebf3bc52efd41382fb56ce728
locdayne <trantanloc20045@gmail.com>
2024-02-13 12:26:35 +0700

    init project

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'My tiny luv (expectation 🥰)',
+    introDesc: `I've prepared a special gift for you, a token of my affection and a promise of the unwavering support. Currently, I know we just a friend, but i wanna make something for my lover, who sympathize my struggle and willing to accepct my weakness. Until then, let this gift serve as a symbol of my hopeful anticipation.`,
+    btnIntro: 'hehehe',
+    title: 'Welcome to my companyyyy, inspired from amiuoi cooperation ',
+    desc: 'Thay em cuteee khumm',
+    btnYes: 'Cute quá tr ròi <3',
+    btnNo: 'Ùa cũng tạm ',
+    question: 'Hiện tại em muốn tìm cổ đông "xây dựng" cty này, a đồng ý chớ',
+    btnReply: 'apply neeeee',
+    reply: 'Đồng ý chớ, cutee z màaaaa',
+    mess: 'Hic hic củm ơn anh nhìu',
+    messDesc: 'Em còn muốn thay đổi bản thân mình rất rất nhiều, và nếu em may mắn em có thể tìm được động lực của riêng em',
+    btnAccept: 'Anh bít ròiiii',
+    messLink: 'http://fb.com' 
+}
